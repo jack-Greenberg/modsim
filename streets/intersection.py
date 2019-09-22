9126d513ea740b5053c7b430f9e1b5bc341051e6
jack-Greenberg <j.lester.greenberg@gmail.com>
2019-09-22 13:14:58 -0400

    Generalize light intersection axes

--- streets/intersection.py
+++ streets/intersection.py
@@ -1,7 +1,7 @@
 class Intersection(object):
     """
     position (tuple (x, y)): Position of the intersection on the grid
-    light_state (bool): True if cars allowed horizontally, False if cars allowed vertially
+    light_state (bool): True if cars allowed to pass on primary axis, False if cars allowed on secondary axis
     light_timing (int): Number of seconds in a timing cycle 
     cars (tuple (n, e, s, w)): Number of cars at each of the parts of the intersection
     """
