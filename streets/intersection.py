ebf79c2195d8d24c221e44a39545fb473b30dd94
jack-Greenberg <j.lester.greenberg@gmail.com>
2019-09-24 12:18:12 -0400

    Clean up intersection

--- streets/intersection.py
+++ streets/intersection.py
@@ -3,7 +3,6 @@ class Intersection(object):
     position (tuple (x, y)): Position of the intersection on the grid
     light_state (bool): True if cars allowed to pass on primary axis, False if cars allowed on secondary axis
     light_timing (int): Number of seconds in a timing cycle 
-    cars (tuple (n, e, s, w)): Number of cars at each of the parts of the intersection
     """
     def __init__(self, position, light_timing, light_state=True):
         self.position = position
